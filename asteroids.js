62cf206f848cdd11c63dbbc0e77dfcb4bb899485
Graeme Stuart <ggstuart@dmu.ac.uk>
2015-02-18 19:42:18 +0000

    tabs

--- asteroids.js
+++ asteroids.js
@@ -1,43 +1,45 @@
 function extend(ChildClass, ParentClass) {
-	var parent = new ParentClass();
-	ChildClass.prototype = parent;
-	ChildClass.prototype.super = parent.constructor;
-	ChildClass.prototype.constructor = ChildClass;
+  var parent = new ParentClass();
+  ChildClass.prototype = parent;
+  ChildClass.prototype.super = parent.constructor;
+  ChildClass.prototype.constructor = ChildClass;
 }
 function random_position(canvas) {
-	return {
-		x: Math.random() * canvas.width,
-		y: Math.random() * canvas.height
-	}
+  return {
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height
+  }
+}
 }
 function middle(canvas) {
-	return {
-		x: canvas.width / 2,
-		y: canvas.height / 2
-	}
+  return {
+    x: canvas.width / 2,
+    y: canvas.height / 2
+  }
 }
 function random_velocity(canvas) {
-	return {
-		x: (Math.random() - 0.5) * canvas.width * 0.01,
-		y: (Math.random() - 0.5) * canvas.height * 0.01
-	}
+  return {
+    x: (Math.random() - 0.5) * canvas.width * 0.01,
+    y: (Math.random() - 0.5) * canvas.height * 0.01
+  }
+}
 }
 function random_rotation() {
-	return Math.random() * 2 * Math.PI * 0.01;
+  return Math.random() * 2 * Math.PI * 0.01;
 }
 function distance_between(obj1, obj2) {
-	return Math.sqrt(Math.pow(obj1.position.x - obj2.position.x,2) + Math.pow(obj1.position.y - obj2.position.y,2));
+  return Math.sqrt(Math.pow(obj1.position.x - obj2.position.x,2) + Math.pow(obj1.position.y - obj2.position.y,2));
 }
 // THE GAME===========================
 
 function Game(canvas) {
-	this.canvas = canvas;
-	this.c = this.canvas.getContext("2d");
-	this.ship = new Ship(this, this.canvas);
-	this.restart();
+  this.canvas = canvas;
+  this.c = this.canvas.getContext("2d");
+  this.ship = new Ship(this, this.canvas);
+  this.restart();
 }
 Game.prototype.restart = function() {
-	this.objects = [];
+  this.objects = [];
   this.level = 1;
   this.add_asteroids();
   this.ship.position = middle(this.canvas);
@@ -64,7 +66,7 @@ Game.prototype.update = function() {
 	}
 }
 Game.prototype.refresh = function() {
-	this.c.clearRect ( 0 , 0 , this.canvas.width, this.canvas.height );
+  this.c.clearRect (0, 0, this.canvas.width, this.canvas.height);
 	for (var i=0;i<this.objects.length;i++) {
 		this.objects[i].refresh(this.c);
 	}
@@ -195,165 +197,166 @@ Asteroid = function(canvas, mass, position, velocity, rotation_speed) {
 	this.density = 0.2 + Math.random() * 0.1;
 	this.volume = this.mass / this.density;
 	this.radius = Math.pow(this.volume / (4/3)*Math.PI, 1/3); //assumes unit density i.e. mass === volume
-	this.bumpiness = 0.25 + Math.random() * 0.5;
-	this.randoms = [];
-	var vertices = (5 + Math.random() * 10).toFixed()
-	for(var i=0; i<vertices; i++) {
-		this.randoms.push(this.radius * (1 - this.bumpiness) + this.radius * this.bumpiness * Math.random());
-	}
+  this.bumpiness = 0.25 + Math.random() * 0.5;
+  this.randoms = [];
+  var vertices = (5 + Math.random() * 10).toFixed()
+  for(var i=0; i<vertices; i++) {
+    this.randoms.push(this.radius * (1 - this.bumpiness) + this.radius * this.bumpiness * Math.random());
+  }
 }
 extend(Asteroid, Mass);
 
 
 Asteroid.prototype.refresh = function(c) {
-	var angle = Math.PI * 2 / this.randoms.length;
-	c.save();
-	c.translate(this.position.x, this.position.y);
-	c.rotate(this.angle);
-	c.beginPath();
-	c.moveTo(0, this.randoms[this.randoms.length - 1]);
-	for (var i=0;i<this.randoms.length;i++) {
-		c.rotate(angle);
-		c.lineTo(0, this.randoms[i]);
-	}
-	c.lineWidth = 2;
-	c.strokeStyle = '#00a300';
-	c.stroke();
-	c.restore();
+  var angle = Math.PI * 2 / this.randoms.length;
+  c.save();
+  c.translate(this.position.x, this.position.y);
+  c.rotate(this.angle);
+  c.beginPath();
+  c.moveTo(0, this.randoms[this.randoms.length - 1]);
+  for (var i=0;i<this.randoms.length;i++) {
+    c.rotate(angle);
+    c.lineTo(0, this.randoms[i]);
+  }
+  c.lineWidth = 2;
+  c.strokeStyle = '#00a300';
+  c.fillStyle = '#00a300';
+  c.font = "10px Arial";
+  c.fillText(this.mass.toString(), -5, 0);
+  c.stroke();
+  c.restore();
 }
 
 // THE SHIP===========================
 
 Ship = function(game) {
-	this.super(game.canvas, 1000, {x: game.canvas.width / 2, y: game.canvas.height / 2}, {x: 0, y: 0}, 0)
-	this.game = game;
+  this.super(game.canvas, 1000, {x: game.canvas.width / 2, y: game.canvas.height / 2}, {x: 0, y: 0}, 0)
+  this.game = game;
 
-	this.mainThruster = false;
-	this.leftBooster = false;
-	this.rightBooster = false;
-	this.retroThruster = false;
-	this.trigger = false;
+  this.mainThruster = false;
+  this.leftBooster = false;
+  this.rightBooster = false;
+  this.retroThruster = false;
+  this.trigger = false;
 
-	this.life = 100;
-	this.radius = 5;
+  this.life = 100;
+  this.radius = 5;
 
-	this.power = 70;
-	this.torque = 2.5;
-	this.friction = 0.995;
+  this.power = 70;
+  this.torque = 2.5;
+  this.friction = 0.995;
 
-	this.shooting_power = 200;
-	this.reload_time = 15;
-	this.reload_in = 0;
-	this.projectile_mass = 100;
-	this.projectile_life = 200;
-	this.projectile_impact = 1;
+  this.shooting_power = 200;
+  this.reload_time = 15;
+  this.reload_in = 0;
+  this.projectile_mass = 100;
+  this.projectile_life = 200;
+  this.projectile_impact = 100;
 }
 extend(Ship, Mass);
 Ship.prototype.refresh = function(c) {
-	var unit = this.radius;
-	c.save();
-	c.translate(this.position.x, this.position.y);
-	c.rotate(-this.angle);
-	c.beginPath();
-	c.moveTo(0, 2*unit);
-	c.rotate(Math.PI * 2 / 3);
-	c.lineTo(0, 1*unit);
-	c.rotate(Math.PI * 2 / 3);
-	c.lineTo(0, 1*unit);
-	c.rotate(Math.PI * 2 / 3);
-	c.lineTo(0, 2*unit);
-	c.lineWidth = 2;
-	c.strokeStyle = '#00a300';
-	c.stroke();
-	c.restore();
+  var unit = this.radius;
+  c.save();
+  c.translate(this.position.x, this.position.y);
+  c.rotate(-this.angle);
+  c.beginPath();
+  c.moveTo(0, 2*unit);
+  c.rotate(Math.PI * 2 / 3);
+  c.lineTo(0, 1*unit);
+  c.rotate(Math.PI * 2 / 3);
+  c.lineTo(0, 1*unit);
+  c.rotate(Math.PI * 2 / 3);
+  c.lineTo(0, 2*unit);
+  c.lineWidth = 2;
+  c.strokeStyle = '#00a300';
+  c.stroke();
+  c.restore();
 }
 Ship.prototype.apply_friction = function() {
-	this.rotation_speed *= this.friction; //retard the rotation for gameplay reasons
-	this.velocity.x *= this.friction; //retard the speed for gameplay reasons
-	this.velocity.y *= this.friction; //retard the speed for gameplay reasons
+  this.rotation_speed *= this.friction; //retard the rotation for gameplay reasons
+  this.velocity.x *= this.friction; //retard the speed for gameplay reasons
+  this.velocity.y *= this.friction; //retard the speed for gameplay reasons
 }
 Ship.prototype.update = function() {
-	this.apply_torque(this.torque * (this.leftBooster - this.rightBooster));
-	var force_magnitude = this.power * (this.mainThruster - this.retroThruster);
-	this.apply_force(this.angle, force_magnitude);
-	if (this.trigger && this.reload_in === 0) {
-		var projectile = new Projectile(this);
-		projectile.apply_force(this.angle, this.shooting_power);
-		this.apply_force(this.angle - Math.PI, this.shooting_power);
+  this.apply_torque(this.torque * (this.leftBooster - this.rightBooster));
+  var force_magnitude = this.power * (this.mainThruster - this.retroThruster);
+  this.apply_force(this.angle, force_magnitude);
+  if (this.trigger && this.reload_in === 0) {
+    var projectile = new Projectile(this);
+    projectile.apply_force(this.angle, this.shooting_power);
+    this.apply_force(this.angle - Math.PI, this.shooting_power);
 		this.game.objects.push(projectile);
-		this.reload_in = this.reload_time;
-	}
-	if(this.reload_in > 0) this.reload_in -= 1;
-	this.apply_friction();
-	Mass.prototype.update.apply(this, arguments);
-}
-
 Ship.prototype.projectile_velocity = function() {
 	return {
 		x: this.velocity.x * 2,
 		y: this.velocity.y * 2
 	}
+    this.reload_in = this.reload_time;
+  }
+  if(this.reload_in > 0) this.reload_in -= 1;
+  this.apply_friction();
+  Mass.prototype.update.apply(this, arguments);
 }
 Ship.prototype.launch_position = function() {
-	return {
-		x: this.position.x + this.velocity.x + Math.sin(this.angle) * this.radius * 2,
-		y: this.position.y + this.velocity.y + Math.cos(this.angle) * this.radius * 2
-	};
+  return {
+    x: this.position.x + this.velocity.x + Math.sin(this.angle) * this.radius * 2,
+    y: this.position.y + this.velocity.y + Math.cos(this.angle) * this.radius * 2
+  };
 }
 
 
 // PROJECTILES===========================
 
 Projectile = function(ship) {
-	this.super(ship.canvas, ship.projectile_mass, ship.launch_position(), {x: ship.velocity.x, y: ship.velocity.y}, 0)
-	this.impact = ship.projectile_impact;
-	this.life = ship.projectile_life;
-	this.radius = 5;
+  this.super(ship.canvas, ship.projectile_mass, ship.launch_position(), {x: ship.velocity.x, y: ship.velocity.y}, 0)
+  this.impact = ship.projectile_impact;
+  this.life = ship.projectile_life;
+  this.radius = 1;
 }
 extend(Projectile, Mass);
 Projectile.prototype.update = function() {
-	this.life -= 1;
-	if (this.life <= 0) {
-		this.delete_me = true;
-	}
-	Mass.prototype.update.apply(this, arguments);	
+  this.life -= 1;
+  if (this.life <= 0) {
+    this.delete_me = true;
+  }
+  Mass.prototype.update.apply(this, arguments);  
 }
 Projectile.prototype.refresh = function(c) {
-	c.save();
-	c.translate(this.position.x, this.position.y);
-	c.beginPath();
-	c.arc(0,0,Math.min(this.life / 100, 2),0,2*Math.PI);
-	c.lineWidth = 1;
-	c.strokeStyle = '#00a300';
-	c.stroke();
-	c.restore();
+  c.save();
+  c.translate(this.position.x, this.position.y);
+  c.beginPath();
+  c.arc(0,0,Math.min(this.life / 100, 2),0,2*Math.PI);
+  c.lineWidth = 1;
+  c.strokeStyle = '#00a300';
+  c.stroke();
+  c.restore();
 }
 
 
 // PARTICLES===========================
 
 Particle = function(game, position, velocity) {
-	this.super(game.canvas, game.particle_mass(), position, velocity, 0)
-	this.life = game.particle_life;
-	game.objects.push(this);
+  this.super(game.canvas, game.particle_mass(), position, velocity, 0)
+  this.life = game.particle_life;
+  game.objects.push(this);
 }
 extend(Particle, Mass);
 Particle.prototype.update = function() {
-	this.life -= 1;
-	if (this.life <= 0) {
-		this.delete_me = true;
-	}
-	Mass.prototype.update.apply(this, arguments);	
+  this.life -= 1;
+  if (this.life <= 0) {
+    this.delete_me = true;
+  }
+  Mass.prototype.update.apply(this, arguments);  
 }
 Particle.prototype.refresh = function(c) {
-	c.save();
-	c.translate(this.position.x, this.position.y);
-	c.beginPath();
-	c.arc(0,0,Math.min(this.life / 100, 2),0,2*Math.PI);
-	c.lineWidth = 1;
-	c.fillStyle = '#dd3300';
-	c.fill();
-	c.restore();
+  c.save();
+  c.translate(this.position.x, this.position.y);
+  c.beginPath();
+  c.arc(0,0,Math.min(this.life / 100, 2),0,2*Math.PI);
+  c.lineWidth = 1;
+  c.fillStyle = '#dd3300';
+  c.fill();
+  c.restore();
 }
 
 
